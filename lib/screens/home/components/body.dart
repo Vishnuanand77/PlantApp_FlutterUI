beccf3f576e7a46c6ece3b6ee440606cf0095549
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-04 22:21:45 +0530

    Added more button

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_ui/constants.dart';
 
 import 'package:plant_app_ui/screens/home/components/header_with_searchbar.dart';
 
@@ -12,18 +13,83 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBar(size: size),
-          Container(
-            height: 24,
-            child: Stack(
-              children: [
-                Text(
-                  "Recomended",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+          TitleWithMoreButton(
+            title: "Recomended",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithMoreButton extends StatelessWidget {
+  const TitleWithMoreButton({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(text: title),
+          Spacer(),
+          FlatButton(
+            color: kPrimaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            onPressed: press,
+            child: Text(
+              "More",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
             ),
           )
         ],

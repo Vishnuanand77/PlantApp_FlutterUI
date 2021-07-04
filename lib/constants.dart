12f0870a228cda037044b4e0ba859992b587e193
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-04 21:43:44 +0530

    Updated kBgColor Hex

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 //Colors :
 const kPrimaryColor = Color(0xFF0C9869);
 const kTextColor = Color(0xFF3C4046);
-const kBgColor = Color(0xFF9F8FD);
+const kBgColor = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 20.0;

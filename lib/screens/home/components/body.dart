d0722ce5278243f93c944cd3f190d7eb6e309cc9
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-04 21:54:37 +0530

    Created background and added search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app_ui/constants.dart';
 
 class Body extends StatelessWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +11,61 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: size.height*0.2,
+          height: size.height * 0.2,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: kPrimaryColor.withOpacity(0.5),
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SvgPicture.asset("assets/icons/search.svg"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         )
       ],
     );
